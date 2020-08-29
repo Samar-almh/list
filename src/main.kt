b89861480bbf200eef4dc93e53b20cdbc816a7ba
Samar.almh <49631872+Samar-almh@users.noreply.github.com>
2020-08-29 22:03:49 +0300

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var list = listOf<Int>(8,4,7,1,2,3,0,5,6)
+    println("--")
+    println("sort the list")
+    println("    " + list.sorted())
+    println("sort the list")
+    println("   " + list.sortedDescending())
+    println("--")
+}
